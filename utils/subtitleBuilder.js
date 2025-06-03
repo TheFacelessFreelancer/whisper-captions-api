c772ca9e2b125c16c45cbd44769dfe447fb5cf0f
TheFacelessFreelancer <thefacelessfreelancer@gmail.com>
2025-06-03 23:07:43 +0200

    Update subtitleBuilder.js

--- utils/subtitleBuilder.js
+++ utils/subtitleBuilder.js
@@ -1,32 +1,59 @@
-
 import fs from 'fs/promises';
 
-export async function buildAssSubtitle({ subtitlePath, fontName, fontSize, fontColor, position }) {
-  const positions = {
-    top: '1',
-    bottom: '2',
-    center: '8'
+export async function buildAssSubtitle({
+  subtitlePath,
+  fontName = 'Arial',
+  fontSize = 40,
+  fontColor = 'FFFFFF',
+  outlineColor = '000000',
+  bold = false,
+  italic = false,
+  underline = false,
+  alignment = 'bottom-safe',
+  marginV = 100,
+  blockStyle = false,
+  blockColor = '000000',
+  animation = 'none',
+  shadow = 0,
+}) {
+  const alignmentMap = {
+    'top-safe': 7,
+    'bottom-safe': 2,
+    'center-safe': 5,
   };
-  const placement = positions[position] || '2';
+  const placement = alignmentMap[alignment] || 2;
 
-  const assStyle = `
-[Script Info]
+  const boolToInt = (value) => value ? -1 : 0;
+
+  const assStyle = `[Script Info]
 ScriptType: v4.00+
 PlayResX: 1920
 PlayResY: 1080
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,\${fontName},\${fontSize},&H\${fontColor},&H000000,&H000000,0,0,0,0,100,100,0,0,1,2,0,\${placement},30,30,30,1
+Style: Default,${fontName},${fontSize},&H00${fontColor},&H00${outlineColor},&H00${blockColor},${boolToInt(bold)},${boolToInt(italic)},${boolToInt(underline)},0,100,100,0,0,1,1,${shadow},${placement},30,30,${marginV},1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
-\`;
+`;
 
   const rawSubs = await fs.readFile(subtitlePath, 'utf-8');
-  const subLines = rawSubs.split('\n').filter(Boolean).map(line => \`Dialogue: 0,\${line}\`);
-  const finalSub = assStyle + subLines.join('\n');
+  const srtLines = rawSubs.split('\n');
 
+  let subs = [];
+  for (let i = 0; i < srtLines.length; i++) {
+    if (srtLines[i].match(/\d+:\d+:\d+,\d+/)) {
+      const startTime = srtLines[i].split(' --> ')[0].replace(',', '.');
+      const endTime = srtLines[i].split(' --> ')[1].replace(',', '.');
+      const text = srtLines[i + 1] || '';
+      const effect = animation !== 'none' ? animation : '';
+      subs.push(`Dialogue: 0,${startTime},${endTime},Default,,0,0,0,${effect},${text}`);
+      i += 1;
+    }
+  }
+
+  const finalSub = assStyle + subs.join('\n');
   const assPath = subtitlePath.replace('.srt', '.ass');
   await fs.writeFile(assPath, finalSub, 'utf-8');
   return assPath;
