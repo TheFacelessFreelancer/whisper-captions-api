6d437b68aeb4044c68ffd4ecf64c71756fccde0e
TheFacelessFreelancer <thefacelessfreelancer@gmail.com>
2025-06-05 08:40:28 +0200

    Update subtitleBuilder.js

--- utils/subtitleBuilder.js
+++ utils/subtitleBuilder.js
@@ -1,55 +1,48 @@
-import fs from 'fs';
-
-export function buildAssSubtitle(events, options) {
+export function buildAssSubtitle(events, options = {}) {
   const {
     fontSize = 42,
-    fontColor = '&H00FFFFFF', // White
+    fontColor = '&H00FFFFFF',
     fontName = 'Arial',
-    outlineColor = '&H00000000', // Black
+    outlineColor = '&H00000000',
     outlineWidth = 4,
-    alignment = 8, // Top center
-    marginV = 300,
+    alignment = 8, // top-center
+    marginV = 300, // roughly 1/3 from top
     animation = true,
     box = true,
-    boxColor = '&H00000000' // Black box
+    boxColor = '&H64000000' // semi-transparent black
   } = options;
 
-  const style = `Style: Default,${fontName},${fontSize},&H00FFFFFF,&H00FFFFFF,${outlineColor},${boxColor},0,0,0,${outlineWidth},0,0,1,2,${alignment},20,20,${marginV},1`;
-
-  const header = `[Script Info]
+  const style = `
+[Script Info]
 ScriptType: v4.00+
-PlayResX: 720
-PlayResY: 1280
+PlayResX: 1080
+PlayResY: 1920
 
 [V4+ Styles]
-Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-${style}
+Format: Name, Fontname, Fontsize, PrimaryColour, BackColour, OutlineColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
+Style: Default,${fontName},${fontSize},${fontColor},${box ? boxColor : '&H00000000'},${outlineColor},0,0,0,0,100,100,0,0,1,${outlineWidth},0,${alignment},30,30,${marginV},1
 
 [Events]
-Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text`; 
+Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
+`;
 
-  const eventsFormatted = events.map((e, index) => {
-    const start = formatTime(e.start);
-    const end = formatTime(e.end);
-    const text = animation
-      ? `{\fad(200,200)}${e.text}`
-      : e.text;
+  const formatTime = (seconds) => {
+    const h = Math.floor(seconds / 3600).toString().padStart(1, '0');
+    const m = Math.floor((seconds % 3600) / 60).toString().padStart(2, '0');
+    const s = Math.floor(seconds % 60).toString().padStart(2, '0');
+    const cs = Math.floor((seconds % 1) * 100).toString().padStart(2, '0');
+    return `${h}:${m}:${s}.${cs}`;
+  };
 
-    return `Dialogue: 0,${start},${end},Default,,0,0,0,,${text}`;
-  });
+  const subtitleBody = events
+    .map(({ start, end, text }) => {
+      const startTime = formatTime(start);
+      const endTime = formatTime(end);
+      const effect = animation ? 'Fade(0,0,500,0)' : '';
+      const cleanText = text.replace(/\r?\n|\r/g, ' ');
+      return `Dialogue: 0,${startTime},${endTime},Default,,0,0,0,${effect},${cleanText}`;
+    })
+    .join('\n');
 
-  return `${header}
-${eventsFormatted.join('\n')}`;
-}
-
-function formatTime(seconds) {
-  const hrs = Math.floor(seconds / 3600);
-  const mins = Math.floor((seconds % 3600) / 60);
-  const secs = Math.floor(seconds % 60);
-  const cs = Math.floor((seconds % 1) * 100);
-  return `${pad(hrs)}:${pad(mins)}:${pad(secs)}.${pad(cs)}`;
-}
-
-function pad(num) {
-  return num.toString().padStart(2, '0');
+  return style + subtitleBody;
 }
