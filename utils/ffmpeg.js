730a19593101b2cbf2c6cf4baa74f94ea5af9564
TheFacelessFreelancer <thefacelessfreelancer@gmail.com>
2025-06-05 15:03:54 +0200

    Update ffmpeg.js

--- utils/ffmpeg.js
+++ utils/ffmpeg.js
@@ -1,14 +1,18 @@
+// utils/ffmpeg.js
+
 import { exec } from 'child_process';
 import { promisify } from 'util';
 
 const execAsync = promisify(exec);
 
-export async function extractAudio(videoPath, audioPath) {
-  const command = `ffmpeg -i ${videoPath} -q:a 0 -map a ${audioPath} -y`;
-  return execAsync(command);
-}
+export const extractAudio = async (videoPath, audioPath) => {
+  console.log('🔊 Extracting audio with FFmpeg...');
+  const command = `ffmpeg -i "${videoPath}" -vn -acodec libmp3lame -ar 44100 -b:a 192k "${audioPath}" -y`;
+  await execAsync(command);
+};
 
-export async function renderVideoWithSubtitles(videoPath, subtitlePath, outputPath) {
-  const command = `ffmpeg -i "${videoPath}" -vf "ass='${subtitlePath}'" -c:a copy "${outputPath}" -y`;
-  return execAsync(command);
-}
+export const renderVideoWithSubtitles = async (videoPath, subtitlePath, outputPath) => {
+  console.log('🎬 Rendering final video with subtitles...');
+  const command = `ffmpeg -i "${videoPath}" -vf "ass='${subtitlePath}'" -c:v libx264 -preset fast -crf 23 -c:a copy "${outputPath}" -y`;
+  await execAsync(command);
+};
