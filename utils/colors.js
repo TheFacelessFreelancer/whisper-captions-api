5be109cfbafadee884b5d5e590475558b67c089b
TheFacelessFreelancer <thefacelessfreelancer@gmail.com>
2025-06-10 09:59:05 +0200

    Create colors.js

---
+++ utils/colors.js
@@ -0,0 +1,14 @@
+// utils/colors.js
+
+function hexToASS(hex) {
+  if (!hex) return "&H00000000";
+  const match = hex.match(/^#?([a-f\d]{6})$/i);
+  if (!match) return "&H00000000";
+  const rgb = match[1];
+  const r = rgb.slice(0, 2);
+  const g = rgb.slice(2, 4);
+  const b = rgb.slice(4, 6);
+  return `&H00${b}${g}${r}`;
+}
+
+module.exports = { hexToASS };
