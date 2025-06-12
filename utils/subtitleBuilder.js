4f33d12d5bce9d93be72b7d7f5575e938c6de855
TheFacelessFreelancer <thefacelessfreelancer@gmail.com>
2025-06-12 08:58:07 +0200

    Update subtitleBuilder.js

--- utils/subtitleBuilder.js
+++ utils/subtitleBuilder.js
@@ -22,18 +22,18 @@ export async function buildSubtitlesFile({
 [Script Info]
 Title: Captions
 ScriptType: v4.00+
-PlayResX: 1920
-PlayResY: 1920
+PlayResX: 720
+PlayResY: 1280
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,${fontName},${fontSize},${fontColor},&H00000000,${outlineColor},${boxColor},0,0,0,0,100,100,0,0,1,${outlineWidth},${shadow},2,10,10,10,1
+Style: Default,${fontName},${fontSize},${fontColor},&H00000000,${outlineColor},${boxColor},0,0,0,0,100,100,${lineSpacing},0,1,${outlineWidth},${shadow},2,10,10,50,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
 `;
 
-  // Placeholder dialogue line — customize later
+  // Placeholder dialogue line — to be replaced dynamically in later versions
   const dialogue = `Dialogue: 0,0:00:00.00,0:00:03.00,Default,,0,0,0,,Hello world!`;
 
   const content = styles + dialogue;
