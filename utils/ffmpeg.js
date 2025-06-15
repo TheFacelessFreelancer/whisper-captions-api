2b59cac1f75fa532f2c467704939504a2f6e882b
TheFacelessFreelancer <thefacelessfreelancer@gmail.com>
2025-06-15 21:29:35 +0200

    Update ffmpeg.js

--- utils/ffmpeg.js
+++ utils/ffmpeg.js
@@ -18,6 +18,7 @@
 // ────────────────────────────────────────────────
 import { exec } from 'child_process';
 import { promisify } from 'util';
+import path from 'path';
 const execAsync = promisify(exec);
 
 // ────────────────────────────────────────────────
