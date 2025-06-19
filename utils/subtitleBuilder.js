761e05a128cbb43f7d3b59810bcaf9d0c20baa3e
TheFacelessFreelancer <thefacelessfreelancer@gmail.com>
2025-06-19 19:58:00 +0200

    Update subtitleBuilder.js

--- utils/subtitleBuilder.js
+++ utils/subtitleBuilder.js
@@ -80,7 +80,7 @@ export async function buildSubtitlesFile({
 
     const boxColorAss = box ? hexToASSWithAlpha(boxColor, boxAlpha) : '&H00000000';
     const finalOutlineColor = hexToASS(boxOutlineColorHex);
-    const finalOutlineWidth = boxOutlineWidth || 0;
+    const finalOutlineWidth = box ? Math.max(1, boxOutlineWidth || 0) : 0;
 
     const style = `
 [Script Info]
